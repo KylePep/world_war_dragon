05c701379c6dc4485047ee19ce4878b27a438e9f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-08-11 05:16:56 -0600

    stats and inventory in dropdown

--- WorldWarDragon.client/src/components/OffBar.vue
+++ WorldWarDragon.client/src/components/OffBar.vue
@@ -77,70 +77,52 @@
             <Login />
           </div>
 
-          <div class="accordion accordion-flush" id="accordionFlushExample">
-            <div class="accordion-item">
-              <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                  data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                  Stats
-                </button>
-              </h2>
-              <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body stat-block">
-
-                  <div class="mdi mdi-circle-multiple " title="Gold">
-                    Gold: {{ account?.gold }}
-                  </div>
-                  <div class="mdi mdi-medal" title="Experience">
-                    Experience: {{ account?.valor - appState.account?.valorSpent }}
-                  </div>
-                  <div class="mdi mdi-heart" title="Health">
-                    Health: {{ account?.health }}
-                    <!-- ( {{ appState.healthMod[appState.activeRoom.id] }} ) -->
-                  </div>
-                  <div class="mdi mdi-weight-lifter" title="Power">
-                    Power: {{ account?.power }}
-                    <!-- ( {{ appState.powerMod[appState.activeRoom.id] }} ) -->
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                  data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                  Inventory
-                </button>
-              </h2>
-              <div id="flush-collapseTwo" class="accordion-collapse collapse " data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body stat-block">
-                  <div class="mdi mdi-sword-cross text-danger" title="Attack">
-                    Attack: {{ account?.attack }} ( {{ account?.attackAid }} )
-                  </div>
-                  <div class="mdi mdi-shield-sun text-info" title="Shield">
-                    Shield: {{ account?.shield }} ( {{ account?.shieldAid }} )
-                  </div>
-                  <div class="mdi mdi-bottle-tonic-plus text-success" title="Heal">
-                    Heal: {{ account?.heal }} ( {{ account?.healAid }} )
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 
-            <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+            <button class="d-flex justify-content-between align-items-center btn btn-dark dropdown-toggle mb-2"
+              data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
+              STATS
             </button>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <ul class="dropdown-menu p-2">
+              <div class="stat-block">
+
+                <div class="mdi mdi-circle-multiple " title="Gold">
+                  Gold: {{ account?.gold }}
+                </div>
+                <div class="mdi mdi-medal" title="Experience">
+                  Experience: {{ account?.valor - appState.account?.valorSpent }}
+                </div>
+                <div class="mdi mdi-heart" title="Health">
+                  Health: {{ account?.health }}
+                  <!-- ( {{ appState.healthMod[appState.activeRoom.id] }} ) -->
+                </div>
+                <div class="mdi mdi-weight-lifter" title="Power">
+                  Power: {{ account?.power }}
+                  <!-- ( {{ appState.powerMod[appState.activeRoom.id] }} ) -->
+                </div>
+              </div>
+            </ul>
+
+            <button class="d-flex justify-content-between align-items-center btn btn-dark dropdown-toggle mb-2"
+              data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
+              Inventory
+            </button>
+            <ul class="dropdown-menu p-2">
+              <div class="stat-block">
+                <div class="mdi mdi-sword-cross text-danger" title="Attack">
+                  Attack: {{ account?.attack }} ( {{ account?.attackAid }} )
+                </div>
+                <div class="mdi mdi-shield-sun text-info" title="Shield">
+                  Shield: {{ account?.shield }} ( {{ account?.shieldAid }} )
+                </div>
+                <div class="mdi mdi-bottle-tonic-plus text-success" title="Heal">
+                  Heal: {{ account?.heal }} ( {{ account?.healAid }} )
+                </div>
+              </div>
             </ul>
 
             <li class="">
-              <router-link class="btn lighten-30 selectable text-uppercase" @click="closeNavbar()"
+              <router-link class="btn text-light selectable text-uppercase" @click="closeNavbar()"
                 :to="{ name: 'Home' }">
                 HOME
               </router-link>
@@ -148,13 +130,13 @@
 
             <li class="">
               <router-link @click="closeNavbar()" :to="{ name: 'Map' }"
-                class="btn lighten-30 selectable text-uppercase">
+                class="btn text-light selectable text-uppercase">
                 Map
               </router-link>
             </li>
 
             <li class="">
-              <router-link class="btn lighten-30 selectable text-uppercase" @click="closeNavbar()"
+              <router-link class="btn text-light selectable text-uppercase" @click="closeNavbar()"
                 :to="{ name: 'Account' }">
                 Character
                 <LevelUpAvailable />
@@ -163,7 +145,7 @@
 
             <li class="">
               <router-link @click="closeNavbar()" :to="{ name: 'Tutorial' }"
-                class="btn lighten-30 selectable text-uppercase">
+                class="btn text-light selectable text-uppercase">
                 Tutorial
               </router-link>
             </li>
@@ -171,21 +153,21 @@
 
             <li class="">
               <router-link @click="closeNavbar()" :to="{ name: 'Score' }"
-                class="btn lighten-30 selectable text-uppercase">
+                class="btn text-light selectable text-uppercase">
                 Hall of Valor
               </router-link>
             </li>
 
             <li class="">
               <router-link @click="closeNavbar()" :to="{ name: 'About' }"
-                class="btn lighten-30 selectable text-uppercase">
+                class="btn text-light selectable text-uppercase">
                 Lore
               </router-link>
             </li>
 
             <li class="">
               <router-link @click="closeNavbar()" :to="{ name: 'RoadMap' }"
-                class="btn lighten-30 selectable text-uppercase">
+                class="btn text-light selectable text-uppercase">
                 Changes
               </router-link>
             </li>
