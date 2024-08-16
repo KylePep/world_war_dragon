c9a140563cc0771f016549e306e1eb5141168422
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-08-16 02:28:52 -0600

    dialogue is revealed as you press continue

--- WorldWarDragon.client/src/components/DialogueModal.vue
+++ WorldWarDragon.client/src/components/DialogueModal.vue
@@ -5,8 +5,9 @@
       <div class="modal-content">
 
         <div class="modal-header">
-          <h1 v-if="account?.id" class="modal-title fs-5" id="dialogueModalLabel">{{ dialogue[pagination].speaker.name
-            }}</h1>
+          <h1 v-if="account?.id" class="modal-title fs-5" id="dialogueModalLabel">{{
+            dialogue[page.pagination].speaker.name
+          }}</h1>
           <button @click="closeCheckSeen()" type="button" class="btn-close" aria-label="Close"></button>
         </div>
 
@@ -14,16 +15,28 @@
           <div class="container-fluid">
 
             <div class="row">
-              <div v-if="account?.id" class="col-6">
+              <!-- <div v-if="account?.id" class="col-6">
                 DialogueLength: {{ dialogue.length }} <br>
                 DialogueLevel: {{ dialogue[pagination].levelRequirement }} <br>
                 Pagination: {{ pagination }} <br>
                 DialogueSeen: {{ dialogueSeen }} <br>
                 {{ dialogue[pagination].messages[0] }} <br>
                 {{ dialogue[pagination].messages[1] }}
+              </div> -->
+              <div v-if="account?.id" class="col-6 bg-dark position-relative pb-5">
+                <div v-for="message, index in dialogue[page.pagination].messages" :key="index">
+                  <div v-if="page.reveal >= index">
+                    {{ message }}
+                  </div>
+                  <div @click="handleReveal()" class="position-absolute bottom-0 end-0 btn btn-secondary"
+                    v-else-if="page.reveal >= index - 1">
+                    Continue
+                  </div>
+                  <div v-else></div>
+                </div>
               </div>
               <div v-if="account?.id" class="col-6">
-                <img class="img-fluid rounded" :src="dialogue[pagination].speaker.img" alt="">
+                <img class="img-fluid rounded" :src="dialogue[page.pagination].speaker.img" alt="">
               </div>
             </div>
 
@@ -31,10 +44,10 @@
         </div>
 
         <div class="modal-footer d-flex justify-content-between">
-          <button v-if="pagination > 1" @click="handlePagination(-1)" type="button"
+          <button v-if="page.pagination > 1" @click="handlePagination(-1)" type="button"
             class="btn btn-secondary">Previous</button>
           <button v-else class="btn btn-dark">Previous</button>
-          <button v-if="pagination < dialogue.length - 1" @click="handlePagination(1)" type="button"
+          <button v-if="page.pagination < dialogue.length - 1" @click="handlePagination(1)" type="button"
             class="btn btn-secondary">Next</button>
           <button v-else class="btn btn-dark">Next</button>
         </div>
@@ -56,17 +69,17 @@ import { accountService } from "../services/AccountService.js";
 export default {
   setup() {
     const account = computed(() => AppState.account)
-    const pagination = ref(0)
+    const page = ref({ pagination: 0, reveal: 0 })
     const dialogueSeen = ref(0)
 
     watchEffect(() => {
       if (account.value && account.value.dialogueSeen !== undefined) {
-        pagination.value = account.value.dialogueSeen;
+        page.value.pagination = account.value.dialogueSeen;
         dialogueSeen.value = account.value.dialogueSeen;
       }
     });
     return {
-      pagination,
+      page,
       dialogueSeen,
       identity: computed(() => AppState.identity),
       account: computed(() => AppState.account),
@@ -76,14 +89,18 @@ export default {
       }),
 
       handlePagination(direction) {
-        const newValue = pagination.value + direction;
+        const newValue = page.value.pagination + direction;
 
         if (newValue >= 1 && newValue <= this.dialogue.length - 1) {
-          pagination.value = newValue;
+          page.value.pagination = newValue;
+          page.value.reveal = 0;
           if (newValue > dialogueSeen.value) {
             dialogueSeen.value = newValue;
           }
         }
+      },
+      handleReveal() {
+        page.value.reveal++
       },
 
       async updateDialogueSeen() {
