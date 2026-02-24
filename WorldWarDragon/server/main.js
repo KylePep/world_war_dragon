4468b54e306090bba05ea6ce646c36ef92ac304e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2026-02-23 17:40:27 -0700

    change server serving frontend path

--- WorldWarDragon/server/main.js
+++ WorldWarDragon/server/main.js
@@ -1,35 +1,37 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url'
 import dotenv from 'dotenv';
 dotenv.config();
 import { createServer } from 'http'
 import { DbConnection } from './db/DbConfig.js'
-// import { socketProvider } from './SocketProvider.js'
 import { Startup } from './Startup.js'
 import { logger } from './utils/Logger.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 
 // create server & socketServer
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(process.cwd(), 'WorldWarDragon/server/public')));
+const clientDistPath = path.join(__dirname, '../../WorldWarDragon.client/dist')
+
+app.use(express.static(clientDistPath))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'WorldWarDragon/server/public/index.html'));
-});
+  res.sendFile(path.join(clientDistPath, 'index.html'))
+})
 
 if (process.env.NODE_ENV == 'dev') {
   // @ts-ignore
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
 }
 
 const httpServer = createServer(app)
 Startup.ConfigureGlobalMiddleware(app)
 Startup.ConfigureRoutes(app)
-
-// Establish Socket
-// socketProvider.initialize(httpServer)
 
 // Connect to Atlas MongoDB
 DbConnection.connect().catch(err => {
